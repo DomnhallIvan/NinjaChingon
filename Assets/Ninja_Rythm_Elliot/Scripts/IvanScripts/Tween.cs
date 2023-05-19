bda7fd1ebc923d036aaf2765dfff5975ae430211
LeinadDH <daniel_navarrete_05@hotmail.com>
2023-05-19 13:01:04 -0600

    UI&LeanTweenImplementation

---
+++ Assets/Ninja_Rythm_Elliot/Scripts/IvanScripts/Tween.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tween : MonoBehaviour
+{
+    [SerializeField]
+
+    GameObject levelSucces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.scale(levelSucces, new Vector3(1.5f, 1.5f, 1.5f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.moveLocal(levelSucces, new Vector3(-30f, 747f, 2f), .7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.scale(levelSucces, new Vector3(1f, 1f, 1f), 2f).setDelay(1.7f).setEase(LeanTweenType.easeInOutCubic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
