0ad7c596bca476a13005df899f507d93cd76810f
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-06-05 17:31:39 -0600

    1level finish

--- Assets/Ninja_Rythm_Elliot/Scripts/Scripts/NoteScoreManager.cs
+++ Assets/Ninja_Rythm_Elliot/Scripts/Scripts/NoteScoreManager.cs
@@ -6,18 +6,24 @@ using TMPro;
 public class NoteScoreManager : MonoBehaviour
 {
     public TMP_Text ScoreText;
+   
     public float PlayerScore;
+  
     // Start is called before the first frame update
     void Start()
     {
         PlayerScore = 0;
+       
         ScoreText.text = "Score " + PlayerScore;
+       
     }
 
     
     public void ChangeExScore(float Puntos)
     {
         PlayerScore = PlayerScore + Puntos;
+        
         ScoreText.text = "Score " + PlayerScore;
+        
     }
 }
