253f289d207f8237e0172b0c00ffc05dcf6a04c7
GrittYvan <cdmx2125@cdmx.edu.mx>
2023-06-05 11:54:08 -0600

    Fix LeenyTweeny

--- Assets/Ninja_Rythm_Elliot/Scripts/IvanScripts/Tween.cs
+++ Assets/Ninja_Rythm_Elliot/Scripts/IvanScripts/Tween.cs
@@ -1,17 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using TMPro;
+
 
 public class Tween : MonoBehaviour
 {
+    //Objeto que referencio para determinar posicións
+    public TMP_Text TextPosition;
     [SerializeField]
-
     GameObject levelSucces;
+    
+   
     // Start is called before the first frame update
     void Start()
     {
         LeanTween.scale(levelSucces, new Vector3(1.5f, 1.5f, 1.5f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
-        LeanTween.moveLocal(levelSucces, new Vector3(-30f, 650f, 2f), .7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
+       // LeanTween.moveLocal(levelSucces, new Vector3(-30f, 650f, 2f), .7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.moveLocal(levelSucces, new Vector3(TextPosition.transform.position.x, TextPosition.transform.position.y, 2f), .7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
         LeanTween.scale(levelSucces, new Vector3(1f, 1f, 1f), 2f).setDelay(1.7f).setEase(LeanTweenType.easeInOutCubic);
     }
 
