06037e409a662dbac3701f6c48019d1201f72743
AtraxxDev <gametraxworks@gmail.com>
2023-06-05 08:20:44 -0600

    Arreglado canvas y ya para build

--- Assets/Ninja_Rythm_Elliot/Scripts/SetActiveButtons.cs
+++ Assets/Ninja_Rythm_Elliot/Scripts/SetActiveButtons.cs
@@ -6,15 +6,24 @@ public class SetActiveButtons : MonoBehaviour
 {
     public GameObject Panel;
     public GameObject Buttons;
+    public GameObject Start;
+    public GameObject Name;
+    public GameObject Creditspanel;
     public void ActiveB()
     {
         Buttons.SetActive(false);
+        Start.SetActive(false);
+        Name.SetActive(false);
         Panel.SetActive(true);
+        Creditspanel.SetActive(true);
     }
     public void DesactiveB()
     {
         Buttons.SetActive(true);
+        Start.SetActive(true);
+        Name.SetActive(true);
         Panel.SetActive(false);
+        Creditspanel?.SetActive(false);
     }
 
 }
