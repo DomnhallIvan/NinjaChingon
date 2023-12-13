fb534c60b7d549827bd9e49937821d7fc1d9e16b
Ivan PM <cdmx2125@amerike.edu.mx>
2023-12-13 01:31:31 -0600

    YA FUNCIONA SIIIIIIIII, LA ESTA COSA HIGHSCORE WUUUH

--- Assets/Ninja_Rythm_Elliot/Scripts/Scripts/NoteScoreManager.cs
+++ Assets/Ninja_Rythm_Elliot/Scripts/Scripts/NoteScoreManager.cs
@@ -6,24 +6,41 @@ using TMPro;
 public class NoteScoreManager : MonoBehaviour
 {
     public TMP_Text ScoreText;
-   
-    public float PlayerScore;
+    [SerializeField] private LevelNumber lvNum;
+    public int PlayerScore;
   
     // Start is called before the first frame update
     void Start()
     {
-        PlayerScore = 0;
-       
-        ScoreText.text = "" + PlayerScore;
-       
+        PlayerScore = 0;       
+        ScoreText.text = "" + PlayerScore;       
     }
 
-    
-    public void ChangeExScore(float Puntos)
+    public void ChangeExScore(int Puntos)
     {
         PlayerScore = PlayerScore + Puntos;
-        
+
         ScoreText.text = "" + PlayerScore;
-        
+
+        CheckHighScore();
+
+    }
+
+    //Mio
+    public void FixedUpdate()
+    {
+       
+       // Debug.Log(PlayerScore);
+        //Debug.Log(PlayerPrefs.GetInt("HighScore"+levelNumber));
+    }
+
+    //Aqui digo si el PlayerScore es mayor al HighScore del nivel, entonces se pone un nuevo HighScore de ese nivel, y si no hay valor se pone 0 por default
+    public void CheckHighScore()
+    {
+        //Debug.Log(PlayerScore);
+       if(PlayerScore> PlayerPrefs.GetInt("HighScore"+lvNum.levelNumber))
+        {
+            PlayerPrefs.SetInt("HighScore"+lvNum.levelNumber, PlayerScore);
+        }
     }
 }
